b1b76e0da2b85f01a69b2644dad32c1ae8145821
João Pailol da Silva Lima <joaopalima91@gmail.com>
2022-07-19 17:27:21 -0300

    java script

---
+++ revisao-logica/tipos-primitivos.js
@@ -0,0 +1,23 @@
+// strings, number(int, float), boolean
+
+
+let minhaVar = "minha string";
+let minhavar2 = 'minha "string" com aspas simples';
+var minhavar3 = 'minha template literal';
+
+let idade = 40;
+//let msg = "eu possuo " + idade + " anos";
+let msg = 'eu possuo +(idade)+ anos'; // não está rodando
+
+console.log(msg);
+console.log("Hello " + "World!");
+
+// verificar tipo da vairável
+console.log(typeof msg, typeof idade);
+
+// dado numérico (inteiro, real)
+
+const n1 = 10
+const n2 = 1.1
+console.log('o tipo de n1 é ' + (typeof n1));
+console.log('o tipo de n2 é ' + (typeof n2));
