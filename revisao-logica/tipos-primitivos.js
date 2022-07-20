45a3b6ae6e4b4a802d0353b95779c9973f1ecace
João Paulo da SIlva lima <joaopalima91@gmail.com>
2022-07-19 21:42:42 -0300

    continuação primitivos

--- revisao-logica/tipos-primitivos.js
+++ revisao-logica/tipos-primitivos.js
@@ -1,13 +1,12 @@
 // strings, number(int, float), boolean
 
-
 let minhaVar = "minha string";
 let minhavar2 = 'minha "string" com aspas simples';
 var minhavar3 = 'minha template literal';
 
 let idade = 40;
 //let msg = "eu possuo " + idade + " anos";
-let msg = 'eu possuo +(idade)+ anos'; // não está rodando
+let msg = 'eu possuo ${idade} anos'; // não está rodando
 
 console.log(msg);
 console.log("Hello " + "World!");
@@ -21,3 +20,14 @@ const n1 = 10
 const n2 = 1.1
 console.log('o tipo de n1 é ' + (typeof n1));
 console.log('o tipo de n2 é ' + (typeof n2));
+
+// booleanos
+
+const isValid = true;
+console.log('isValid: '+isValid)
+
+// valor undefined
+
+let varTeste;
+console.log(varTeste);
+console.log(typeof varTeste)
